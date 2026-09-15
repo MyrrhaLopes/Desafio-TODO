13c41b7778b2cabb475fc933e672030bfa49d451
Myrrha Lopes <lorenzolopes223@gmail.com>
2026-09-15 14:05:28 -0300

    done user authN and authZ with middleware using cookie sessions

--- src/backend/app.ts
+++ src/backend/app.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import { errorHandler } from "./http/middleware/errorHandler.middleware";
 import { taskRouter } from "./http/features/tasks/task.route";
+import { userRouter } from "./http/features/users/user.route";
 
 const app = express();
 
 app.use(express.json()); //parseia a sequência de bytes como json em toda requisição
 
 app.use('api/v1/',taskRouter);
-
+app.use('api/v1/',userRouter)
 // error handler global vai no final, depois de todas as rotas
 app.use(errorHandler);
 
