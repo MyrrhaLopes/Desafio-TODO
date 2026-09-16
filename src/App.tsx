ccec00ed4d493b0e47edfefebbfd8da47090a877
Myrrha Lopes <lorenzolopes223@gmail.com>
2026-09-16 01:52:48 -0300

    fixed protected routes flashing for a second

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { RouterProvider } from "@tanstack/react-router";
-import { useEffect } from "react";
 import { router } from "./frontend/router";
 import useAuth from "./frontend/hooks/useAuth";
 
 export default function App() {
   const { data: user, isLoading } = useAuth();
 
-  useEffect(() => {
-    router.invalidate();
-  }, [user, isLoading]);
+  if (isLoading) return null;
 
   return (
     <RouterProvider router={router} context={{ auth: { user: user ?? null, isLoading } }} />
